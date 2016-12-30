8c30f0001b51d494ce16c5c1320ad4feb7ad81de
Hery Vandoro <vandorohery99@gmail.com>
2016-12-30 16:21:03 +0700

    Add files via upload
    Add sample of Stack Implementation

---
+++ StackDataStructure.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <Windows.h>
+
+struct book{
+	char studentname[30];
+	struct book *next, *prev;
+}*head, *tail, *current;
+
+void print(){
+	if(head!=NULL){
+		current=head;
+		while(current!=NULL){
+			printf("%s -> ", current->studentname);
+			current=current->next;
+		}
+	}else{
+		printf("No Data");
+	}
+	printf("\n\n");
+};
+
+void pushHead(char name[]){
+	current = (struct book *)malloc(sizeof book);
+	strcpy(current->studentname, name);
+	current->next = current->prev = NULL;
+
+	if(head==NULL){
+		head=tail=current;
+	}else{
+		head->prev=current;
+		current->next=head;
+		head=current;
+	}
+}
+
+void popHead(){
+	if(head!=NULL){
+		current=head;
+		head=head->next;
+		free(current);
+	}
+}
+
+void main(){
+	int menu;
+	char studentname[30];
+	print();
+	do{
+		do{
+			system("cls");
+			print();
+			printf("1. Stack\n");
+			printf("2. UnStack\n");
+			printf("3. Exit\n");
+			printf("Choose : "); scanf("%d", &menu); fflush(stdin);
+		}while(menu<1 || menu>3);
+		switch(menu){
+			case 1 : 
+				printf("Input Student Name : "); scanf("%s", studentname); fflush(stdin);
+				pushHead(studentname);
+				break;
+			case 2 : 
+				popHead();
+				break;
+		}
+	}while(menu!=3);
+}
